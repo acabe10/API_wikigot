50ced787b23d47098b920c045723468e905d0f19
acabe10 <alejandrocabezab@gmail.com>
2019-05-24 14:21:27 +0400

    apprun

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 URL_BASE = 'https://anapioficeandfire.com/api/'
 
 port = os.environ['PORT']
-app.run('0.0.0.0',int(port), debug=True)
 language = 'es-ES'
 
 @app.route('/',methods = ['GET'])
 def inicio():
-	return render_template("templates/index.html")
+	return render_template("index.html")
+
+app.run('0.0.0.0',int(port), debug=True)
