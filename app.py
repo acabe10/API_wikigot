0292cc6b37af4d46636701a9517e0a02afdc8632
acabe10 <alejandrocabezab@gmail.com>
2019-05-24 13:31:18 +0400

    probando

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,abort,request
+import os,requests
+app = Flask(__name__)
+
+URL_BASE = 'https://anapioficeandfire.com/api/'
+
+port = os.environ['PORT']
+
+language = 'es-ES'
+
+@app.route('/',methods = ['GET'])
+def inicio():
+   def inicio():
+        return render_template("inicio.html")
+
+@app.route('/busqueda', methods = ['GET', 'POST'])
+def busqueda():
+        if request.method == 'GET':
+            return render_template("busqueda.html")
+
+@app.route('/contacto')
+def contacto():
+    return render_template("contact.html")
+
+@app.route('/listas')
+def listas():
+    return render_template("listas.html")
+
+app.run('0.0.0.0',int(port), debug=True)
+# app.run(debug=True)
