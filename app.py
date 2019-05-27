ef69556f20220359c5f2b19c10099f9bad68f048
acabe10 <alejandrocabezab@gmail.com>
2019-05-27 18:35:09 +0400

    libros

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)	
+URL_BASE ="https://anapioficeandfire.com/api/"
 
-#URL_BASE = 'https://anapioficeandfire.com/api/'
 #port = os.environ['PORT']
 #language = 'es-ES'
 
 @app.route('/')
 def inicio():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route('/libros/')
 def libros():
-    return render_template("libros.html")
+	r=requests.get(URL_BASE+'books/')
+	if r.status_code == 200:
+		doc = r.json()
+		return render_template("libros.html",datos=doc)
 
 @app.route('/casas/')
 def casas():
